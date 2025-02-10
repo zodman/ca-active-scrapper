6c86afbdf5cfa72a450a27c276caa6dc9445ac95
Andres Vargas <avargas@nicoyalife.com>
2025-02-09 23:49:12 -0500

    update

--- display.py
+++ display.py
@@ -11,8 +11,7 @@ with open("output.json") as f:
 filtered = []
 for i in results:
     r = Schema.from_dict(i)
-    if "pickleball" in r.name.lower():
-        filtered.append(r)
+    filtered.append(r)
 
 now = datetime.datetime.now()
 
@@ -31,18 +30,19 @@ filtered = sorted(
     key=filter_by_date,
     reverse=True,
 )
+
 for i in filtered:
     if i.activity_online_start_time:
         d = parse(i.activity_online_start_time)
         two_weeks_later = now + datetime.timedelta(days=14)
-        # if d >= two_weeks_later:
-        # continue
+        if d >= two_weeks_later:
+            continue
         f = timeago.format(d, now)
     else:
         f = "now"
 
-    print(f"""{i.name}, open: {f}
-            {i.date_range if "to" in i.date_range else i.date_range } {parse(i.date_range).strftime('%A') if "to" not in i.date_range else ''} {i.time_range}
+    print(f"""{i.name},  {i.openings} avail,  open: {f}  ages: {i.ages}
+            {parse(i.date_range).strftime('%A') if "to" not in i.date_range else ''} {timeago.format(parse(i.date_range.split("to")[0]), now) if "to"  in i.date_range else timeago.format(parse(i.date_range), now)} {i.date_range } {i.time_range}
             {i.location["label"]}
           {i.detail_url}
         
--- main.py
+++ main.py
@@ -2,26 +2,52 @@ import requests
 import json
 import jq
 import pydash as py_
+import sys
 
 
+s = requests.Session()
 waterloo = "activewaterloo"
 kitchener = "activekitchener"
 
 """
-{"activity_search_pattern":{"skills":[],"time_after_str":"","days_of_week":null,"activity_select_param":2,"center_ids":[],"time_before_str":"","open_spots":null,"activity_id":null,"activity_category_ids":[],"date_before":"","min_age":null,"date_after":"","activity_type_ids":[],"site_ids":[],"for_map":false,"geographic_area_ids":[],"season_ids":[],"activity_department_ids":[],"activity_other_category_ids":[],"child_season_ids":[],"activity_keyword":"","instructor_ids":[],"max_age":null,"custom_price_from":"","custom_price_to":""},"activity_transfer_pattern":{}}
+{"activity_search_pattern":{
+"skills":[],"time_after_str":"","days_of_week":None,"activity_select_param":2,"center_ids":[],"time_before_str":"","open_spots":None,"activity_id":None,"activity_category_ids":[],"date_before":"","min_age":None,"date_after":"","activity_type_ids":[],"site_ids":[],"for_map":false,"geographic_area_ids":[],"season_ids":[],"activity_department_ids":[],"activity_other_category_ids":[],"child_season_ids":[],"activity_keyword":"","instructor_ids":[],"max_age":None,"custom_price_from":"","custom_price_to":""},"activity_transfer_pattern":{}}
 """
 results = []
+activity = sys.argv[1]
 
-for location in [waterloo, kitchener]:
+for location in [kitchener, waterloo]:
     url = f"https://anc.ca.apm.activecommunities.com/{location}/rest/activities/list"
     page = 1
     while True:
-        resp = requests.post(
+        resp = s.post(
             url,
             json={
                 "activity_search_pattern": {
-                    "activity_keyword": "pickleball",
-                    "time_after_str": "17:00:00",
+                    "activity_select_param": 2,
+                    "activity_keyword": activity,
+                    "time_after_str": "16:00:00",
+                    "days_of_week": None,
+                    "center_ids": [],
+                    "time_before_str": "",
+                    "open_spots": 1,
+                    "activity_id": None,
+                    "activity_category_ids": [],
+                    "date_before": "",
+                    "min_age": None,
+                    "date_after": "",
+                    "activity_type_ids": [],
+                    "site_ids": [],
+                    "for_map": False,
+                    "geographic_area_ids": [],
+                    "season_ids": [],
+                    "activity_department_ids": [],
+                    "activity_other_category_ids": [],
+                    "child_season_ids": [],
+                    "instructor_ids": [],
+                    "max_age": None,
+                    "custom_price_from": "",
+                    "custom_price_to": "",
                 },
             },
             headers={
@@ -56,10 +82,10 @@ for location in [waterloo, kitchener]:
                 total_open = i["total_open"]
 
                 loc = py_.get(i, "location.label")
-                # print(
-                #     f"{location}:  {i['name']} {i['id']} available {openings}/{total_open} "
-                #     f"dates: {i['date_range']} - {i['days_of_week']} time: {i['time_range']} ages:{i['ages']} location: {loc}"
-                # )
+                print(
+                    f"{location}:  {i['name']} {i['id']} available {openings}/{total_open} "
+                    f"dates: {i['date_range']} - {i['days_of_week']} time: {i['time_range']} ages:{i['ages']} location: {loc}"
+                )
                 results.append(i)
 
 with open("output.json", "w") as f:
