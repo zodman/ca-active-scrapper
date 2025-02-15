11043bf12ac5b0b03e71b1d5548a57f34ff8c96e
Andres Vargas <avargas@nicoyalife.com>
2025-02-14 20:12:10 -0500

    update

--- deploy.py
+++ deploy.py
@@ -1,5 +1,5 @@
 from fabric import Connection
-from patchwork.transfer import rsync
+from patchwork.transfers import rsync
 import os
 
 c = Connection(host="zodman@100.120.174.80")
@@ -9,9 +9,8 @@ BASE_DIR = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 
 APP_DIR = f"~/apps/{BASE_DIR}"
 
-rsync(c, ".", APP_DIR, exclude=[".venv"])
+rsync(c, ".", APP_DIR, exclude=[".venv", ".git"])
 
 c.run(f"mkdir -p {APP_DIR}")
-
 with c.cd(APP_DIR):
-    pass
+    c.run("~/.local/bin/mise install")
